b07bdd4052bc4ca428c60675e825894ba0817858
Ivan Guo <ivanguo248@gmail.com>
2023-10-22 16:10:02 -0700

    added a courses.txt and also the getcourses.py

--- getcourses.py
+++ getcourses.py
@@ -1,4 +1,8 @@
 import requests
+import openai
+
+# Set your API key
+openai.api_key = 'YOUR_API_KEY'
 
 # Define the URL of the API you want to call
 api_url = 'https://ubcgrades.com/api/v3/subjects/UBCV'
@@ -20,9 +24,20 @@ if response.status_code == 200:
             courses = []
             for obj in data:
                 courses.append(obj['course_title'])
-            print(courses)
+            # with open("courses.txt", "a") as txt_file:
+            #     for line in courses:
+            #         txt_file.write("".join(line) + "\n")
         else:
             print(f"API call failed with status code {subject_res.status_code}")
 
+    print(len(courses))
+
 else:
     print(f"API call failed with status code {response.status_code}")
+
+# Define the API request
+response = openai.Completion.create(
+    engine="gpt-3.5-turbo",
+    prompt="Generate a list of courses: \n1. Course A\n2. Course B\n3. Course C\nGroup these courses into categories:",
+    max_tokens=50  # Adjust the number of tokens based on the desired output length
+)
