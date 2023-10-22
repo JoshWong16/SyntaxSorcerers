a2ff572779e0f54bdaeb021e0f0ae3912aca591c
Ivan Guo <ivanguo248@gmail.com>
2023-10-22 15:22:34 -0700

    updated getcourses

--- getcourses.py
+++ getcourses.py
@@ -1,19 +1,28 @@
 import requests
 
 # Define the URL of the API you want to call
-api_url = 'https://ubcgrades.com/api/v3/courses/UBCV/MATH'
-
-# Make a GET request to the API
+api_url = 'https://ubcgrades.com/api/v3/subjects/UBCV'
 response = requests.get(api_url)
 
-# Check the response status code
 if response.status_code == 200:
-    # API call was successful
     data = response.json()
-    courses = []
-    for obj in data:
-        courses.append(obj['course_title'])
-    print(courses)
+    subjects = []
+
+    for d in data:
+        subjects.append(d['subject'])
+    
+    for subject in subjects:
+        subject_url = f'https://ubcgrades.com/api/v3/courses/UBCV/{subject}'
+        subject_res = requests.get(subject_url)
+
+        if subject_res.status_code == 200:
+            data = subject_res.json()
+            courses = []
+            for obj in data:
+                courses.append(obj['course_title'])
+            print(courses)
+        else:
+            print(f"API call failed with status code {subject_res.status_code}")
+
 else:
-    # API call was not successful
     print(f"API call failed with status code {response.status_code}")
