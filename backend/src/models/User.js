dab4f2ad00a209e0d52148f4b359ef491214f3d9
Ivan Guo <ivanguo248@gmail.com>
2023-10-22 12:51:26 -0700

    Added user model code

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,15 +6,42 @@ class User {
     }
 
     async getUser(id) {
-        // TODO: Implement
+        try {
+            const user = await this.collection.findOne({ userId: id });
+            return user;
+        } catch (error) {
+            throw error;
+        }
     }
 
     async createUser(user) {
-        // TODO: Implement
+        try {
+            const result = await this.collection.insertOne(user);
+            return result.insertedId;
+        } catch (error) {
+            throw error;
+        }
     }
 
     async updateUser(userId, updateData) {
-        // TODO: Implement
+        try {
+            const result = await this.collection.updateOne(
+                { userId: userId },
+                { $set: updateData }
+            );
+            return result.matchedCount > 0;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteUser(userId) {
+        try {
+            const result = await this.collection.deleteOne({ userId : userId });
+            return result.deletedCount > 0;
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
