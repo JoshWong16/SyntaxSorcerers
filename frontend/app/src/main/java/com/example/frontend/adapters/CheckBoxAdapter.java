aaf68eccec15e32c32b51b691886197f1cc95c63
Michele Mai <45471647+michele8231@users.noreply.github.com>
2023-10-30 21:36:44 -0700

    add more chatgpt comments

--- frontend/app/src/main/java/com/example/frontend/adapters/CheckBoxAdapter.java
+++ frontend/app/src/main/java/com/example/frontend/adapters/CheckBoxAdapter.java
@@ -15,26 +15,31 @@ public class CheckBoxAdapter extends BaseAdapter {
     private List<CheckBox> checkBoxList;
     private Context context;
 
+    /* ChatGPT usage: Yes */
     public CheckBoxAdapter(Context context, List<CheckBox> checkBoxList) {
         this.context = context;
         this.checkBoxList = checkBoxList;
     }
 
+    /* ChatGPT usage: Yes */
     @Override
     public int getCount() {
         return checkBoxList.size();
     }
 
+    /* ChatGPT usage: Yes */
     @Override
     public Object getItem(int position) {
         return checkBoxList.get(position);
     }
 
+    /* ChatGPT usage: Yes */
     @Override
     public long getItemId(int position) {
         return position;
     }
 
+    /* ChatGPT usage: Yes */
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         CheckBox checkBox = checkBoxList.get(position);
