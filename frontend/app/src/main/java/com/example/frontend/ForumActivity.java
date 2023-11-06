ea3f8d7eb6f45c8374ead91582f75260a9f67092
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-11-05 21:55:28 -0800

    fixed codacy issues

--- frontend/app/src/main/java/com/example/frontend/ForumActivity.java
+++ frontend/app/src/main/java/com/example/frontend/ForumActivity.java
@@ -3,7 +3,6 @@ package com.example.frontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,7 +28,6 @@ import com.google.gson.JsonObject;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class ForumActivity extends AppCompatActivity {
 
