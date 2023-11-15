e908205e25249477817893e90a8f225d452bbee9
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-11-14 18:25:04 -0800

    added userId to comment

--- backend/src/helpers/manipulateCommentOutput.js
+++ backend/src/helpers/manipulateCommentOutput.js
@@ -6,6 +6,7 @@ export default function manipulateCommentOutput(comments) {
             postId: comment.postId,
             content: comment.content,
             writtenBy: comment.username,
+            userId: comment.writtenBy,
             dateWritten: comment.dateWritten
         };
     });
