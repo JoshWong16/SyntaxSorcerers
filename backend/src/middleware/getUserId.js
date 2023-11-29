1f23c154e0b4f9aabc0acfa5babd35b7b9a8728f
Michele Mai <45471647+michele8231@users.noreply.github.com>
2023-11-28 20:08:25 -0800

    remove banned user from middleware

--- backend/src/middleware/getUserId.js
+++ backend/src/middleware/getUserId.js
@@ -3,7 +3,7 @@ import Banned from "../models/Banned.js";
 /* ChatGPT usage: No */
 const getUserId = async (req, res, next) => {
     const model = new Banned();
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer' && !(await model.getBannedUser(req.headers.authorization.split(' ')[1]))) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
         req.userId = req.headers.authorization.split(' ')[1];
         next();
     } else {
