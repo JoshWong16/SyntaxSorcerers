0397438e3defddaf0fec2845947fb99241c94c41
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2023-11-29 18:10:37 -0800

    fixed server request file

--- frontend/app/src/main/java/com/example/frontend/apiwrappers/ServerRequest.java
+++ frontend/app/src/main/java/com/example/frontend/apiwrappers/ServerRequest.java
@@ -28,7 +28,7 @@ import retrofit2.http.Url;
 public class ServerRequest {
 
     public static final String RequestTag = "Server Requests";
-    private static final String BASE_URL = "http://192.168.88.26:8080/";
+    private static final String BASE_URL = "https://grgq6ss4i9.execute-api.us-east-2.amazonaws.com/";
 
     private ApiService apiService;
 
@@ -58,25 +58,25 @@ public class ServerRequest {
 
     /* ChatGPT usage: No */
     public void makeGetRequest(String endpoint, final ServerRequest.ApiRequestListener listener) throws UnsupportedEncodingException {
-        Call<JsonElement> call = apiService.getData(endpoint);
+        Call<JsonElement> call = apiService.getData("/prod" + endpoint);
         callHandler(listener, call);
     }
 
     /* ChatGPT usage: No */
     public void makePostRequest(String endpoint, JsonElement body, final ServerRequest.ApiRequestListener listener) throws UnsupportedEncodingException {
-        Call<JsonElement> call = apiService.postData(endpoint, body);
+        Call<JsonElement> call = apiService.postData("/prod" + endpoint, body);
         callHandler(listener, call);
     }
 
     /* ChatGPT usage: Partial */
     public void makePutRequest(String endpoint, JsonElement body, final ServerRequest.ApiRequestListener listener) throws UnsupportedEncodingException {
-        Call<JsonElement> call = apiService.putData(endpoint, body);
+        Call<JsonElement> call = apiService.putData("/prod" + endpoint, body);
         callHandler(listener, call);
     }
 
     /* ChatGPT usage: No */
     public void makeDeleteRequest(String endpoint, final ServerRequest.ApiRequestListener listener) throws UnsupportedEncodingException {
-        Call<JsonElement> call = apiService.deleteData(endpoint);
+        Call<JsonElement> call = apiService.deleteData("/prod" + endpoint);
         callHandler(listener, call);
     }
 
